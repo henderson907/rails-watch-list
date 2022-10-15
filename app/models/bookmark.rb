cb6242398ddb1f5e19bac4d0390ec9de06741f6c
Freddy Henderson <hendersonsarah12@gmail.com>
2022-10-15 12:18:53 +0200

    implemented validations and associations

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,8 +1,6 @@
 class Bookmark < ApplicationRecord
-  # A bookmark must be linked to a movie and a list, and the [movie, list] pairings should be unique.
-  validates_uniqueness_of :movie_id, scope: :list_id
-
   belongs_to :movie
   belongs_to :list
   validates :comment, presence: true, length: { minimum: 6 }
+  validates_uniqueness_of :movie_id, scope: :list_id
 end
