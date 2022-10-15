b7fb4936a679d6e383015cf9818163b5f4044064
Freddy Henderson <hendersonsarah12@gmail.com>
2022-10-15 11:39:01 +0200

    done all validations, working on associations

---
+++ app/models/movie.rb
@@ -0,0 +1,5 @@
+class Movie < ApplicationRecord
+  has_many :bookmarks, dependent: :restrict_with_exception
+  validates :title, presence: true, uniqueness: true
+  validates :overview, presence: true
+end
